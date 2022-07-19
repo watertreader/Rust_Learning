6b4650d7faa9e4d4755ccd2d3428f2960f2bac40
wch <watertreader@hotmail.com>
2022-07-19 14:41:45 +0800

    rearrangement

--- myproject/src/main.rs
+++ myproject/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 13;
+    let b = 2.3;
+    let c: f32 = 120.0;
+
+    /* YOUR CODE GOES HERE */
+    let af: f32 = a as f32;               // a as f32 is explictly casting it
+    let average = ( af + b + c ) / 3.0;
+
+
+
+    assert_eq!(average, 45.1);
+    println!("Test passed!");
 }
